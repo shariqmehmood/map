32a8277c3486d0494d654101bbfc4529ad2157ea
shariqmehmood <78502043+shariqmehmood@users.noreply.github.com>
2021-11-24 10:45:57 +0500

    Add files via upload
    i am fronend devooloper my name is  shariq mehmood

--- App.js
+++ App.js
@@ -1,38 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-  Button,
-  TextInput,
-  Text,
-  View,
-  Touchable,
-  _ScrollView,
-} from 'react-native';
-import Locati from './src/location';
-import Mapp from './src/map';
-
-import Signup from './src/signup';
+import { StyleSheet, Text, View } from 'react-native';
+import Locationn from './src/location';
+import Maap from './src/mapveiw';
 
 export default function App() {
-  return (
-    <>
-      <ScrollView style={{ flex: 1 }}>
-        <Locati />
-        {/* <Signup />  */}
-        {/* <Mapp /> */}
-      </ScrollView>
-    </>
-  );
+  return <Locationn />;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- src/location.js
+++ src/location.js
@@ -1,69 +1,12 @@
-// import React, { useState, useEffect } from 'react';
-// import { Platform, Text, View, StyleSheet } from 'react-native';
-// import Constants from 'expo-constants';
-// import MapView from 'react-native-maps';
-// import * as Location from 'expo-location';
-
-// export default function Locati() {
-//   const [location, setLocation] = useState(null);
-//   const [errorMsg, setErrorMsg] = useState(null);
-
-//   useEffect(() => {
-//     (async () => {
-//       if (Platform.OS === 'android' && !Constants.isDevice) {
-//         setErrorMsg(
-//           'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
-//         );
-//         return;
-//       }
-//       let { status } = await Location.requestForegroundPermissionsAsync();
-//       if (status !== 'granted') {
-//         setErrorMsg('Permission to access location was denied');
-//         return;
-//       }
-
-//       let location = await Location.getCurrentPositionAsync({});
-//       setLocation(location);
-//     })();
-//   }, []);
-
-//   let text = 'Waiting..';
-//   if (errorMsg) {
-//     text = errorMsg;
-//   } else if (location) {
-//     text = JSON.stringify(location);
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       {/* <Text style={styles.paragraph}>{text}</Text> */}
-//       <MapView style={{ flex: 1, width: 330, height: 700 }} />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     padding: 20,
-//   },
-//   paragraph: {
-//     fontSize: 18,
-//     textAlign: 'center',
-//   },
-// });
-
-import React, { useState, useEffect } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet, Dimensions } from 'react-native';
 import * as Location from 'expo-location';
 
-export default function App() {
+export default function LocationMap() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -73,10 +16,19 @@ export default function App() {
       }
 
       let location = await Location.getCurrentPositionAsync({});
-      console.log(location, 'locationlocation');
       setLocation(location);
+
+      console.log(location.coords.latitude, 'latitude');
+      console.log(location, 'location');
     })();
   }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
     <View style={styles.container}>
@@ -89,16 +41,14 @@ export default function App() {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-          maxZoomLevel={20}
-          minZoomLevel={15}
         >
-          <Marker
+          {/* <Marker
             coordinate={{
               latitude: location.coords.latitude,
               longitude: location.coords.longitude,
             }}
-            title={'Saylani Gulshan Campus'}
-          />
+            image={{ uri: "custom_pin" }}
+          /> */}
         </MapView>
       ) : null}
     </View>
@@ -108,9 +58,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
   },
   map: {
     width: Dimensions.get('window').width,
